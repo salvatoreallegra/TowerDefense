471462069b43b13668b5c3efbb7fda364e0e0746
Salvatore Allegra <salallegra@gmail.com>
2019-04-14 20:05:23 -0700

    Create point class

---
+++ TowerDefense/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
