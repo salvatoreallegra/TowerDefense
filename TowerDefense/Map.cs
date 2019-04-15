4d23a4f505b830ab0f46673504c3fd14ec4f2ad3
Salvatore Allegra <salallegra@gmail.com>
2019-04-15 15:06:32 -0700

    Create 2 points and test if they are on the Map

--- TowerDefense/Map.cs
+++ TowerDefense/Map.cs
@@ -21,7 +21,8 @@ namespace TowerDefense
         //Determine if a point is on a map
         public bool OnMap(Point point)
         {
-            return point.X >= 0 && point.X < Width && point.Y <= 0 && point.Y < Height;
+            return point.X >= 0 && point.X < Width &&
+                   point.Y >= 0 && point.Y < Height;
            
         }
     }
--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -10,8 +10,20 @@ namespace TowerDefense
     {
         static void Main(string[] args)
         {
-            Map map = new Map(7,8);
+            Map map = new Map(8,5);
             int area = map.Width * map.Height;
+
+            Point point = new Point(4, 2);
+            bool isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+
+            point = new Point(8, 5);
+            isOnMap = map.OnMap(point);
+            Console.WriteLine(isOnMap);
+            Console.Read();
+
+
+
         }
     }
 }
