9953ec9bfc7c3de6d90b653a9fad9a228a538078
Salvatore Allegra <salallegra@gmail.com>
2019-04-16 14:22:49 -0700

    Add setters and getters for MapLocation field

--- TowerDefense/Invader.cs
+++ TowerDefense/Invader.cs
@@ -8,5 +8,15 @@ namespace TowerDefense
 {
     class Invader
     {
+        private MapLocation _location;
+
+        public MapLocation GetLocation()
+        {
+            return _location;
+        }
+        public void SetLocation(MapLocation value)
+        {
+            _location = value;
+        }
     }
 }
