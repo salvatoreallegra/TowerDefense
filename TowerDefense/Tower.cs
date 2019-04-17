2eaea32034c90f6127e516247ff9897726dd6a69
Salvatore Allegra <salallegra@gmail.com>
2019-04-16 18:11:47 -0700

    Create FireOnInvaders method

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -8,5 +8,21 @@ namespace TowerDefense
 {
     class Tower
     {
+        private readonly MapLocation _location;
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            int index = 0;
+
+            while (index < invaders.Length)
+            {
+                Invader invader = invaders[index];
+                index++;
+            }
+        }
     }
 }
