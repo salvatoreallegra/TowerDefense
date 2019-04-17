773568a1cf4789df155205bfcfa3e36fd96eeaa9
Salvatore Allegra <salallegra@gmail.com>
2019-04-16 19:26:13 -0700

    Create static random number to reduce accuracy of tower

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -10,10 +10,19 @@ namespace TowerDefense
     {
         private const int range = 1;
         private const int power = 1;
+        private const double _accuracy = .75;
+
+        private static readonly Random _random = new System.Random();
+
         private readonly MapLocation _location;
+
         public Tower(MapLocation location)
         {
             _location = location;
+        }
+        public bool IsSuccessfulShot()
+        {
+            return Tower._random.NextDouble() > _accuracy;
         }
 
         public void FireOnInvaders(Invader[] invaders)
@@ -25,7 +34,19 @@ namespace TowerDefense
                 Invader invader = invaders[index];
                 if (invader.isActive &&_location.InRangeOf(invader.Location, range))
                 {
-                    invader.Decreasehealth(power);
+                    if (IsSuccessfulShot())
+                    {
+                        invader.Decreasehealth(power);
+                        Console.WriteLine("Shot and hit invader");
+                        if (invader.isNeutralized)
+                        {
+                            Console.WriteLine("Neutralized an invader!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shot and missed invader");
+                    }
                     break;
                 }
                 index++;
