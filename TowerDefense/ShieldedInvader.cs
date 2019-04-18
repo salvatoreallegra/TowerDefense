0eb33e002b2a20ec46f0de7b065f5f0be0ab735d
Salvatore Allegra <salallegra@gmail.com>
2019-04-17 17:21:26 -0700

    Create Shielded Invader Subclass

---
+++ TowerDefense/ShieldedInvader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class ShieldedInvader : Invader
+    {
+        public ShieldedInvader(Path path) : base(path)
+        {
+
+        }
+    }
+}
