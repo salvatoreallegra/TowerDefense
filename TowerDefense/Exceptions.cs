bb6b1a423954a332a87bbc7b8a5fa5f16072ce7d
Salvatore Allegra <salallegra@gmail.com>
2019-04-18 15:20:17 -0700

    Re-intruduced code as VS solution got messed up

--- TowerDefense/Exceptions.cs
+++ TowerDefense/Exceptions.cs
@@ -17,8 +17,7 @@ namespace TowerDefense
         }
     }
 
-    class OutOfBoundsException : DefenseException
-    {
+    class OutOfBoundsException : DefenseException    {
         public OutOfBoundsException()
         {
         }
---
+++ TowerDefense/Invader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class Invader
+    {
+        private readonly Path _path;
+        private int _pathStep = 0;
+
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+
+        // True if the invader has reached the end of the path
+        public bool HasScored { get { return _pathStep >= _path.Length; } }
+
+        public int Health { get; private set; } = 2;
+
+        public bool IsNeutralized => Health <= 0;
+
+        public bool IsActive => !(IsNeutralized || HasScored);
+
+        public Invader(Path path)
+        {
+            _path = path;
+        }
+
+        public void Move() => _pathStep += 1;
+
+        public void DecreaseHealth(int factor)
+        {
+            Health -= factor;
+        }
+    }
+}
