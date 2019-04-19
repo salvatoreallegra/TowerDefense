ffd4a22463f56d17b153f4800a0e043e45d3a03e
Salvatore Allegra <salallegra@gmail.com>
2019-04-19 13:48:33 -0700

    Instantiate a shielded invader

--- TowerDefense/Program.cs
+++ TowerDefense/Program.cs
@@ -29,7 +29,7 @@ namespace TowerDefense
 
                 Invader[] invaders =
                 {
-                    new Invader(path),
+                    new ShieldedInvader(path),
                     new Invader(path),
                     new Invader(path),
                     new Invader(path)
