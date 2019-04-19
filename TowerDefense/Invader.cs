bc54b754ba6ace47baac93ee20a4a082d0afa01a
Salvatore Allegra <salallegra@gmail.com>
2019-04-19 13:09:34 -0700

    Make Decrease Health method virtual so we can override in sub class

--- TowerDefense/Invader.cs
+++ TowerDefense/Invader.cs
@@ -29,7 +29,7 @@ namespace TowerDefense
 
         public void Move() => _pathStep += 1;
 
-        public void DecreaseHealth(int factor)
+        public virtual void DecreaseHealth(int factor)
         {
             Health -= factor;
         }
