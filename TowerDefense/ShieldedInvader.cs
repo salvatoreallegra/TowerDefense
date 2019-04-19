ca118eb5ec5ab6b038e0ef47d4758466cc505c19
Salvatore Allegra <salallegra@gmail.com>
2019-04-19 13:17:01 -0700

    Override DecreaseHealth method in superclass and use Rand method

--- TowerDefense/ShieldedInvader.cs
+++ TowerDefense/ShieldedInvader.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace TowerDefense
 {
-    class ShieldedInvader
+    class ShieldedInvader : Invader
     {
+        private static Random _random = new Random();
+
+        public ShieldedInvader(Path path) : base(path)
+        {
+
+        }
+        public override void DecreaseHealth(int factor)
+        {
+           if(_random.NextDouble() < .5)
+            {
+                base.DecreaseHealth(factor);
+            }
+        }
     }
 }
