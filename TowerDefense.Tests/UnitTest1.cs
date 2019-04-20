35360ca585ab8d7098b9f9fff2f037a1278b911f
Salvatore Allegra <salallegra@gmail.com>
2019-04-20 10:01:17 -0700

    Add InternalsVisableTo Test Project

--- TowerDefense.Tests/UnitTest1.cs
+++ TowerDefense.Tests/UnitTest1.cs
@@ -1,18 +1,18 @@
 using System;
 using Xunit;
+using TowerDefense;
 
 namespace TowerDefense.Tests
 {
-    public class Point
+    public class UnitTest1
     {
         [Fact]
         public void PointTest()
         {
             int x = 5;
             int y = 6;
-
             var point = new Point(x, y);
-
+            
         }
     }
 }
