307850412ab6480615917c948601d4d356a5ed94
Salvatore Allegra <salallegra@gmail.com>
2019-04-21 10:56:34 -0700

    Create IInvader interface

---
+++ TowerDefense/IInvader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    interface IInvader
+    {
+         MapLocation Location {get;}
+
+        // True if the invader has reached the end of the path
+         bool HasScored { get; }
+
+          int Health { get; }
+         bool IsNeutralized { get; }
+
+         bool IsActive { get; }
+
+
+
+        void Move();
+
+        void DecreaseHealth(int factor);
+        
+        
+    }
+}
