71d4b9474060bea2d39e5c4f62d24fc603b35a34
Salvatore Allegra <salallegra@gmail.com>
2019-04-20 19:37:51 -0700

    Add an about window and set as startup window

--- TowerDefenseWPF/AboutWindow.xaml.cs
+++ TowerDefenseWPF/AboutWindow.xaml.cs
@@ -10,22 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TowerDefenseWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AboutWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AboutWindow : Window
     {
-        public MainWindow()
+        public AboutWindow()
         {
             InitializeComponent();
         }
 
-     
-    
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+           
+        }
     }
 }
